49fafc401fabfcafb145d5e18863423c9e36c0fe
Christian Coirin <chriatiancoirin@hotmail.com>
2025-05-05 17:37:44 -0500

    added post

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,23 @@ import { connectDB } from './config/db.js';
 const app = express();
 const PORT = 5000;
 
-app.post('/products', (req, res) => {});
+app.post('/products', async (req, res) => {
+    const product = req.body; // Assuming you send product data in the request body
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const newProduct = new Product();
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ message: 'Product created successfully', product: newProduct });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' , error: error.message });
+    }
+});
 
 app.get('/products', (req, res) => {
     res.send('Server is running!');
